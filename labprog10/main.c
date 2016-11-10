409afaaa3181f55e0fb288a0e2d3d583cf01caa4
Praveen-3269 <praveenvajri@gmail.com>
2016-11-10 15:54:00 +0530

    Update main.c

---
+++ labprog10/main.c
@@ -0,0 +1,25 @@
+#include<stdio.h>
+{
+ int main()
+{
+ char ch;
+ printf("enter the character/n");
+ scanf("%c",&ch);
+ switch(ch)
+ {
+ case 'A':printf("vowels");
+ break;
+ case 'E':printf("vowels");
+ break;
+ case 'I':printf("vowels");
+ break;
+ case 'O':printf("vowels")
+ break;
+ case 'U':printf("vowels");
+ break;
+ default:printf("consonants");
+ }
+}
+ return 0;
+
+}
